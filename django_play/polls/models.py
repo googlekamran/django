6d8ac2e7ee258fabcdfc603fbf3506529ca84ed4
googlekamran <ktariq@scs.ryerson.ca>
2018-04-15 14:25:00 -0400

    First commit

--- django_play/polls/models.py
+++ django_play/polls/models.py
@@ -6,10 +6,12 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+#    owner_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
-	
+#    def __str__(self):
+ #       return self.owner_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
